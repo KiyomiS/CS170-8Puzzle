68452bb2d65d34d7dc3678ee4447053d9660c7e2
Kiyomi <kiyomis@ymail.com>
2021-10-27 21:59:31 -0700

    commit on 'master'

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "Puzzle.h"
+using namespace std;
+
+int main() {
+    int option, searchOption;
+    int input;
+    vector<int>start(9);
+    cout << "Select your option:" << endl << " 1. Base Game\n 2. Create your own game" << endl;
+    cin >> option;
+    if(option == 1){
+        cout << "Select search algorithm" << endl;
+        cout << " Option 1: Uniform cost search\n Option 2: A* with the Misplaced Tile heuristic\n Option 3: A* with the Manhattan Distance heuristic" << endl;
+        cin >> searchOption;
+    }
+    if(option == 2){
+        cout << "Please enter your puzzle one integer at a time" << endl;
+        for(int i = 0; i < 9; i++){//take in the puzzle one integer at a time
+            cin >> input;
+            start[i] = input;
+        }
+    }
+    // puzzle base_puzzle = puzzle();
+    // base_puzzle.PrintPuzzle();
+    puzzle new_puzzle = puzzle(start);
+    new_puzzle.PrintPuzzle();
+}
---
+++ puzzle.h
@@ -0,0 +1,42 @@
+#ifndef PUZZLE_H
+#define PUZZLE_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class puzzle {
+    private:
+       vector<int>puzzle_state = {1, 2, 3, 4, 5, 6, 7, 8, 0}; // array of size 9 to hold the puzzle.
+       int cost;
+       puzzle * previous;
+    public:
+        puzzle() {
+            cost = 0;
+            previous = NULL;
+        };
+
+        puzzle(vector<int> input_puzzle){
+            this->puzzle_state = input_puzzle;
+        };
+
+        void PrintPuzzle(){ //outputs the vector
+            for(int i = 0; i < 9; i++){
+                cout << puzzle_state[i] << " ";
+                if(i == 2 || i == 5 || i == 8){
+                    cout << endl;
+                }
+            }
+        }
+};
+
+class row : puzzle {
+
+};
+
+// 1 2 3 
+// 4 5 6
+// 7 8 0
+// 1 2 3 4 5 6 7 8 9
+// up = - 3 indices, down = + 3, left = + 1, right = - 1 
+//include edge cases for each.
+#endif
